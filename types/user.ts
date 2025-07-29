76832c316a734dfd55fb3e20342c65ceca17f866
Choi Jieun <crg1050@gmail.com>
2025-07-29 19:25:50 +0900

    Feat: UserInfoData 타입 추가

--- types/user.ts
+++ types/user.ts
@@ -7,3 +7,11 @@ export interface UserInfo {
 }
 
 export type UserStatus = "initial" | "basic";
+
+export interface UserInfoData {
+  user_id: string;
+  name: string;
+  email: string;
+  created_at: string;
+  status: UserStatus;
+}
