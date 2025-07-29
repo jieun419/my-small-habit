591b6ceef466a1357aecc147991a393b28ecc65b
Choi Jieun <crg1050@gmail.com>
2025-07-29 19:31:06 +0900

    Feat: page 컴포넌트 props, 불필요한 코드 삭제

--- app/(header)/setting/profile/edit/page.tsx
+++ app/(header)/setting/profile/edit/page.tsx
@@ -1,5 +1,10 @@
-const InfoEditPage = () => {
-  return "InfoEditScreen";
+import { getUserInfo } from "@/api/server/user";
+import ProfileEditScreen from "@/features/profileEdit";
+
+const ProfileEditPage = async () => {
+  const { data } = await getUserInfo();
+
+  return <ProfileEditScreen userId={data.user_id as string} />;
 };
 
-export default InfoEditPage;
+export default ProfileEditPage;
--- app/(non-header)/login/page.tsx
+++ app/(non-header)/login/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import LoginScreen from "@/features/login";
 
 const LoginPage = () => {
